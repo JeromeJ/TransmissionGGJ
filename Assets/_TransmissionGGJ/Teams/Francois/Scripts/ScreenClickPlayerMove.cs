dcb9a94482960368a2aabd31fea9e829684a16c4
Exentro <ixr_exentro@hotmail.com>
2018-01-27 11:13:17 +0100

    added navmesh, AI script that link thirdcontroller with navmesh and a script that give the click point as target for the navmesh agent

---
+++ Assets/_TransmissionGGJ/Teams/Francois/Scripts/ScreenClickPlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(AICharacterControl))]
+public class ScreenClickPlayerMove : MonoBehaviour {
+    
+    AICharacterControl m_AIcontroller;
+
+    void Start()
+    {
+        m_AIcontroller = GetComponent<AICharacterControl>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                m_AIcontroller.SetTarget(hit.transform);
+            }
+        }
+    }
+}
