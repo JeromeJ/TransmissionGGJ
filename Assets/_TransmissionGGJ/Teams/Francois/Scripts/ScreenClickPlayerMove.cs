bd7e2c4948880cf4d4cee9df4c601e522c915683
JeromeJ <jerome.jamart.42@gmail.com>
2018-01-28 00:44:28 +0100

    Actually working

--- Assets/_TransmissionGGJ/Teams/Francois/Scripts/ScreenClickPlayerMove.cs
+++ Assets/_TransmissionGGJ/Teams/Francois/Scripts/ScreenClickPlayerMove.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(AICharacterControl))]
-public class ScreenClickPlayerMove : MonoBehaviour {
-    
+public class ScreenClickPlayerMove : MonoBehaviour
+{
+
     AICharacterControl m_AIcontroller;
+    //public Interaction m_IsInteracting = new Interaction();
+    //public class Interaction : UnityEvent<CitizenAgent> { }
 
     void Start()
     {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+        m_citizenAgent = GetComponent<CitizenAgent>();
         m_AIcontroller = GetComponent<AICharacterControl>();
     }
 
@@ -23,6 +30,7 @@ public class ScreenClickPlayerMove : MonoBehaviour {
                 {
                     m_followCitizen = true;
                     m_CitizenToFollow = hit.collider.gameObject.transform;
+                    m_recipientCitizenAgent = m_CitizenToFollow.GetComponent<CitizenAgent>();
                     m_AIcontroller.SetTarget(m_CitizenToFollow.transform.position);
                 }
                 else
@@ -37,9 +45,32 @@ public class ScreenClickPlayerMove : MonoBehaviour {
         {
             Debug.Log(m_CitizenToFollow.transform.position);
             m_AIcontroller.SetTarget(m_CitizenToFollow.position);
+
+            if (!m_navMeshAgent.pathPending)
+            {
+                if (m_navMeshAgent.remainingDistance <= m_navMeshAgent.stoppingDistance)
+                {
+                    // We are near citizen
+                    CitizenAgent otherCitizen = m_CitizenToFollow.GetComponent<CitizenAgent>();
+
+                    if (otherCitizen.CanSwitchToInteracting())
+                    {
+                        CitizenAgent citizenAgent = GetComponent<CitizenAgent>();
+                        citizenAgent.StartConversationWith(otherCitizen, 1.1, _initiator: true);
+                        otherCitizen.StartConversationWith(citizenAgent, 1.1);
+
+                        GetComponent<CitizenManager>().m_IsInteracting.Invoke(otherCitizen);
+                    }
+                }
+            }
         }
+
     }
 
+    private CitizenAgent m_recipientCitizenAgent;
+    private CitizenAgent m_citizenAgent;
+    private NavMeshAgent m_navMeshAgent;
     private Transform m_CitizenToFollow;
     private bool m_followCitizen;
+    private bool m_inTransmission;
 }
