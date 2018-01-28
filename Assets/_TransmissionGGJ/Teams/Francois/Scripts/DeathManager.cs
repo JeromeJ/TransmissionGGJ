546602611b03bda8084178005d4f7c35c467a80e
Exentro <ixr_exentro@hotmail.com>
2018-01-28 13:49:14 +0100

    wip implementing death

--- Assets/_TransmissionGGJ/Teams/Francois/Scripts/DeathManager.cs
+++ Assets/_TransmissionGGJ/Teams/Francois/Scripts/DeathManager.cs
@@ -5,20 +5,37 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class DeathManager : MonoBehaviour
 {
+    TransmissionManager m_transmissionManager;
+    private void Start()
+    {
+        m_transmissionManager = gameObject.GetComponent<TransmissionManager>();
+        if (m_transmissionManager != null)
+        {
+            m_transmissionManager.m_isDead.AddListener(DeathApply);
+        }
+        else Debug.LogError("DeathManager : Can't find TransmissionManager component");
+    }
+
     public void DeathApply()
     {
-        /*
         ThirdPersonCharacter _thirdCaracterController = gameObject.GetComponent<ThirdPersonCharacter>();
-        _thirdCaracterController.ListeningInteraction = false;
-        _thirdCaracterController.TalkingInteraction = false;
-        */
+        if (_thirdCaracterController != null)
+        {
+            _thirdCaracterController.ListeningInteraction = false;
+            _thirdCaracterController.TalkingInteraction = false;
+        }
+        else Debug.LogError("DeathManager : Can't find ThirdPersonCharacter component");
+        
+
         Animator _animator = gameObject.GetComponent<Animator>();
         if (_animator != null) _animator.SetTrigger("Death");
         else Debug.LogError("DeathManager : Can't find Animator component");
 
+
         AICharacterControl _AICharacterControl = gameObject.GetComponent<AICharacterControl>();
         if (_AICharacterControl != null) _AICharacterControl.enabled = false;
         else Debug.LogError("DeathManager : Can't find AICharacterControl component");
+
 
         CitizenAgent _citizenAgent = gameObject.GetComponent<CitizenAgent>();
         if (_citizenAgent != null)
@@ -26,14 +43,6 @@ public class DeathManager : MonoBehaviour
             //_citizenAgent.enabled = false;
             _citizenAgent.m_state = CitizenAgent.E_States.DEAD;
         }
-        else Debug.LogError("DeathManager : Can't find CitizenAgent component");
-        
-        TransmissionManager _transmissionManager = gameObject.GetComponent<TransmissionManager>();
-        if (_transmissionManager != null)
-        {
-            _transmissionManager.enabled = false;
-        }
-        else Debug.LogError("DeathManager : Can't find TransmissionManager component");
-
+        else Debug.LogError("DeathManager : Can't find CitizenAgent component"); 
     }
 }
